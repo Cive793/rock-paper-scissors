53a0e1d74eaa0c968fab68c131b55b35a9b23025
peterstastny <peter.stastny7@gmail.com>
2021-08-26 15:43:44 +0200

    result gestures work

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strict";
 let winner = "computer";
+const choices = ["rock", "paper", "scissors"];
+let computerChoice;
+let playerChoice;
 
 window.addEventListener("load", start);
 
@@ -15,6 +18,13 @@ function start() {
 //player choice
 function getPlayerChoice() {
   console.log("getPlayerChoice");
+
+  /* if ((document.getElementsByClassName(".rock").clicked = true)) {
+    console.log("You picked rock");
+  } else if ((document.getElementsByClassName(".scissors").clicked = true)) {
+    console.log("You picked scissors");
+  } */
+
   makeRandomComputerChoice();
 }
 
@@ -38,6 +48,29 @@ function showResults() {
   console.log("showResults");
   document.querySelector("#player1").classList.remove("shake");
   document.querySelector("#player2").classList.remove("shake");
+  if (playerChoice == "rock") {
+    document.querySelector("#player1").classList.add("rock");
+  }
+
+  if (playerChoice == "paper") {
+    document.querySelector("#player1").classList.add("paper");
+  }
+
+  if (playerChoice == "scissors") {
+    document.querySelector("#player1").classList.add("scissors");
+  }
+
+  if (computerChoice == "rock") {
+    document.querySelector("#player2").classList.add("rock");
+  }
+
+  if (computerChoice == "paper") {
+    document.querySelector("#player2").classList.add("paper");
+  }
+
+  if (computerChoice == "scissors") {
+    document.querySelector("#player2").classList.add("scissors");
+  }
   /* if  */
   determineWinner();
 }
@@ -46,9 +79,27 @@ function showResults() {
 function determineWinner() {
   console.log("determineWinner");
 
-  if (winner === "computer") {
+  if (playerChoice == "rock" && computerChoice == "paper") {
     showLoss();
-  } else if (winner === "user") {
+  }
+
+  if (playerChoice == "rock" && computerChoice == "scissors") {
+    showWin();
+  }
+
+  if (playerChoice == "paper" && computerChoice == "rock") {
+    showWin();
+  }
+
+  if (playerChoice == "paper" && computerChoice == "scissors") {
+    showLoss();
+  }
+
+  if (playerChoice == "scissors" && computerChoice == "rock") {
+    showLoss();
+  }
+
+  if (playerChoice == "scissors" && computerChoice == "paper") {
     showWin();
   } else {
     showDraw();
